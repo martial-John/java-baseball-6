c27e0067a9d19475dc4ed0dfbe3c9cdab2fcdf15
martialJeon <testserver1444@gmail.com>
2026-01-04 15:51:58 +0900

    feat : common -예석아 딱대라 ㅋㅋ

--- src/main/java/baseball/game.java
+++ src/main/java/baseball/game.java
@@ -27,6 +27,7 @@ public class game {
 
         }
         if (strike == 3) {
+            System.out.println("3스트라이크");
             System.out.println("세 개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하시려면 1, 종료하시려면 2를 입력하세요.");
 
@@ -46,7 +47,12 @@ public class game {
 
             }
         }
+
+        if(ball != 0 && strike !=0) {
             System.out.println(ball + "볼" + " " + strike + "스트라이크");
+        }else {
+            System.out.println("낫싱");
+        }
             ball = 0;
             strike = 0;
 
